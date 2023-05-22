69e18dd355078d6f22b374e80c6cd23c3e90b71b
eunseo0423 <kyee0423@gmail.com>
2023-05-22 15:44:54 +0900

    qComment 삭제 수정,qCommentDate vo추가_2023/05/22_eunseo

--- RunUp-back-end/src/main/java/team/spring/runup/question/vo/QuestionComment.java
+++ RunUp-back-end/src/main/java/team/spring/runup/question/vo/QuestionComment.java
@@ -9,61 +9,83 @@ public class QuestionComment {
 		
 	}
 	
-	
-	
+
 	public QuestionComment(int qCommentNum, String qCommentContent, String qCommentWriter, int questionNum,
-			String userUrl) {
+			String qCommentDate, String userUrl) {
 		super();
 		this.qCommentNum = qCommentNum;
 		this.qCommentContent = qCommentContent;
 		this.qCommentWriter = qCommentWriter;
 		this.questionNum = questionNum;
+		this.qCommentDate = qCommentDate;
 		this.userUrl = userUrl;
 	}
 
-	
 
 	private int qCommentNum;
 	private String qCommentContent;
 	private String qCommentWriter;
 	private int questionNum;
+	private String qCommentDate;
 	private String userUrl;
-	
-	
-	
-	
 	public int getqCommentNum() {
 		return qCommentNum;
 	}
+
+
 	public void setqCommentNum(int qCommentNum) {
 		this.qCommentNum = qCommentNum;
 	}
+
+
 	public String getqCommentContent() {
 		return qCommentContent;
 	}
+
+
 	public void setqCommentContent(String qCommentContent) {
 		this.qCommentContent = qCommentContent;
 	}
+
+
 	public String getqCommentWriter() {
 		return qCommentWriter;
 	}
+
+
 	public void setqCommentWriter(String qCommentWriter) {
 		this.qCommentWriter = qCommentWriter;
 	}
+
+
 	public int getQuestionNum() {
 		return questionNum;
 	}
+
+
 	public void setQuestionNum(int questionNum) {
 		this.questionNum = questionNum;
 	}
+
+
+	public String getqCommentDate() {
+		return qCommentDate;
+	}
+
+
+	public void setqCommentDate(String qCommentDate) {
+		this.qCommentDate = qCommentDate;
+	}
+
+
 	public String getUserUrl() {
 		return userUrl;
 	}
+
+
 	public void setUserUrl(String userUrl) {
 		this.userUrl = userUrl;
 	}
-	
-	
 	
 	
 	
